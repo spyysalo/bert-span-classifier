c2c5a57cf71f533fd08c89f7c64d99ba39175b80
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2020-02-05 16:45:23 +0900

    Add NCBI model

--- scripts/get-models.sh
+++ scripts/get-models.sh
@@ -12,14 +12,20 @@ DATADIR="$SCRIPTDIR/../models"
 mkdir -p "$DATADIR"
 
 GOOGLE_BASE_URL="https://storage.googleapis.com/bert_models"
+NCBI_BASE_URL="https://ftp.ncbi.nlm.nih.gov/pub/lu/Suppl/NCBI-BERT"
 
-for url in "$GOOGLE_BASE_URL/2018_10_18/cased_L-12_H-768_A-12.zip"; do
+for url in "$GOOGLE_BASE_URL/2018_10_18/cased_L-12_H-768_A-12.zip" \
+	   "$NCBI_BASE_URL/NCBI_BERT_pubmed_uncased_L-12_H-768_A-12.zip"; do
     b=$(basename "$url" .zip)
     if [ -e "$DATADIR/$b" ]; then
 	echo "$b exists, skipping ..." >&2
     else
 	wget "$url" -O "$DATADIR/$b.zip"
-	unzip "$DATADIR/$b.zip" -d "$DATADIR"
+	if [[ $url == "$NCBI_BASE_URL"* ]]; then
+	    unzip "$DATADIR/$b.zip" -d "$DATADIR/$b"
+	else
+	    unzip "$DATADIR/$b.zip" -d "$DATADIR"
+	fi
 	rm "$DATADIR/$b.zip"
     fi
 done
