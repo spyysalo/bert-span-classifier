d4ca681441fc94c791147799c04ecc26bb5a346f
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2020-06-15 13:33:45 +0300

    Add --vocab argument

--- list_tfrecords.py
+++ list_tfrecords.py
@@ -10,11 +10,25 @@ from argparse import ArgumentParser
 
 def argparser():
     ap = ArgumentParser()
+    ap.add_argument('--vocab', default=None, help='BERT vocabulary')
     ap.add_argument('input_file', nargs='+', help='Input TFRecord file(s)')
     return ap
 
 
+def load_vocab(fn):
+    vocab = []
+    with open(fn) as f:
+        for ln, l in enumerate(f, start=1):
+            l = l.rstrip('\n')
+            vocab.append(l)
+    return vocab
+
+
 def list_tfrecord(fn, options):
+    if options.vocab is None:
+        vocab_map = None
+    else:
+        vocab_map = { i: t for i, t in enumerate(options.vocab) }
     # deprecated
     # for example in tf.compat.v1.io.tf_record_iterator(fn):
     #     print(tf.train.Example.FromString(example))
@@ -25,11 +39,17 @@ def list_tfrecord(fn, options):
         for key, value in sorted(edict.items()):
             # NOTE: assumes int64
             npvalue = np.array(value.int64_list.value)
-            print('{}:\n{}'.format(key, npvalue))
+            if vocab_map is None or key == 'label':
+                print('{} {}:\n{}'.format(key, npvalue.shape, npvalue))
+            else:
+                tokens = [vocab_map[i] for i in npvalue]
+                print('{} {}:\n{}'.format(key, npvalue.shape, ' '.join(tokens)))
 
 
 def main(argv):
     args = argparser().parse_args(argv[1:])
+    if args.vocab is not None:
+        args.vocab = load_vocab(args.vocab)
     for fn in args.input_file:
         list_tfrecord(fn, args)
     return 0
